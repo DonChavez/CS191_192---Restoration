29cea81ad9c57c4544d3f6cb3b1e4858b25465a7
DonChavez <125983971+DonChavez@users.noreply.github.com>
2025-02-08 11:35:57 +0800

    feat: finished tests for HealthComponent

--- Test/tests_HealthComponent.gd
+++ Test/tests_HealthComponent.gd
@@ -21,3 +21,21 @@ func test_HealthComponent_take_damage() -> void:
  health_component.take_damage(10.0)
  assert_float(health_component.health).is_equal(90.0)
  health_component.queue_free()
+
+func test_HealthComponent_take_damage_isAlive() -> void:
+ var health_component =  HealthComponent.new()
+ health_component._ready()
+ health_component.take_damage(90.0)
+ assert_float(health_component.health).is_equal(10.0)
+ assert_bool(health_component.is_alive()).is_equal(true)
+ assert_bool(health_component.is_dead()).is_equal(false)
+ health_component.queue_free()
+
+func test_HealthComponent_take_damage_isDead() -> void:
+ var health_component =  HealthComponent.new()
+ health_component._ready()
+ health_component.take_damage(100.0)
+ assert_float(health_component.health).is_equal(0.0)
+ assert_bool(health_component.is_alive()).is_equal(false)
+ assert_bool(health_component.is_dead()).is_equal(true)
+ health_component.queue_free()
