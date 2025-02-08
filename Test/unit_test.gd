0cea49469f7bce8ee16c751598dfa909086e5567
DonChavez <125983971+DonChavez@users.noreply.github.com>
2025-02-08 11:22:36 +0800

    feature: initial_test_suite_added(Incomplete)
    Will add the other tests for other functions soon

--- Test/unit_test.gd
+++ Test/unit_test.gd
@@ -1,5 +1,23 @@
 class_name ExampleUnitTest
 extends GdUnitTestSuite
 
+# Preloading resources(For per)
+# Note: Should we use const or var for preloads? | I'll use var for now since Components might change
+const HealthComponent = preload("res://Scripts/HealthComponent.gd")
+
+# Sample test
 func test_example():
  assert(true)
+
+func test_HealthComponent_init() -> void:
+ var health_component =  HealthComponent.new()
+ health_component._ready()
+ assert_float(health_component.health).is_equal(100.0)
+ health_component.queue_free()
+	
+func test_HealthComponent_take_damage() -> void:
+ var health_component =  HealthComponent.new()
+ health_component._ready()
+ health_component.take_damage(10.0)
+ assert_float(health_component.health).is_equal(90.0)
+ health_component.queue_free()
