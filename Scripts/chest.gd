15d7c0ecf8934a88a0a428f0407665a34fb1b1b8
ResaiMangco <ramangco1@up.edu.ph>
2025-02-10 17:13:12 +0800

    chest and gatherable

---
+++ Scripts/chest.gd
@@ -0,0 +1,34 @@
+extends StaticBody2D
+
+@onready var panel = $Panel
+@onready var label = $Label
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var inventory_manager = %InventoryManager
+
+var player_nearby = false
+
+func _ready():
+	panel.visible = false
+	label.visible = false
+	
+
+func interact() -> void:
+	panel.visible = !panel.visible
+	
+		
+func _process(_delta: float) -> void:
+	if player_nearby and Input.is_action_just_pressed("interact"):
+		interact()
+	print(player_nearby)
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_nearby = true
+	label.visible = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_nearby = false
+	label.visible = false 
+	panel.visible = false
