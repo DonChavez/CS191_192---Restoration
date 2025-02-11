b233db1e8d2d7cc541a2a2e3c99abdb2fdfb48d7
DonChavez <125983971+DonChavez@users.noreply.github.com>
2025-02-11 18:21:30 +0800

    feat: health_bar_unit_tests
    Added experimental test(commented) out inside new folder(Experimental). Pls don't use it yet(WIP)
    
    Also finished initial health bar tests

---
+++ Experimental/test.gd
@@ -0,0 +1,11 @@
+#@tool
+#extends GdUnitTestSuite
+#
+#const HealthComponent = preload("res://Scripts/HealthComponent.gd")
+#
+#var health_component: HealthComponent
+#
+#func test_sample_run() -> void:
+ #var runner = scene_runner("res://Levels/Level_0.tscn")
+ ##var player_char = runner.invoke()
+ #await await_millis(3000)
--- Test/tests_HealthComponent.gd
+++ Test/tests_HealthComponent.gd
@@ -1,29 +1,29 @@
-class_name ExampleUnitTest
+class_name HealthComponent_UnitTest
 extends GdUnitTestSuite
 
 # Preloading resources(For per)
-# Note: Should we use const or var for preloads? | I'll use var for now since Components might change
-const HealthComponent = preload("res://Scripts/HealthComponent.gd")
+# Note: Should we use const or var for preloads?
+const HealthComponentTest = preload("res://Scripts/HealthComponent.gd")
 
 # Sample test
-func test_example():
- assert(true)
+#func test_example():
+ #assert(true)
 
 func test_HealthComponent_init() -> void:
- var health_component =  HealthComponent.new()
+ var health_component =  HealthComponentTest.new()
  health_component._ready()
  assert_float(health_component.health).is_equal(100.0)
  health_component.queue_free()
 	
 func test_HealthComponent_take_damage() -> void:
- var health_component =  HealthComponent.new()
+ var health_component =  HealthComponentTest.new()
  health_component._ready()
  health_component.take_damage(10.0)
  assert_float(health_component.health).is_equal(90.0)
  health_component.queue_free()
 
 func test_HealthComponent_take_damage_isAlive() -> void:
- var health_component =  HealthComponent.new()
+ var health_component =  HealthComponentTest.new()
  health_component._ready()
  health_component.take_damage(90.0)
  assert_float(health_component.health).is_equal(10.0)
@@ -32,10 +32,15 @@ func test_HealthComponent_take_damage_isAlive() -> void:
  health_component.queue_free()
 
 func test_HealthComponent_take_damage_isDead() -> void:
- var health_component =  HealthComponent.new()
+ var health_component =  HealthComponentTest.new()
  health_component._ready()
  health_component.take_damage(100.0)
  assert_float(health_component.health).is_equal(0.0)
  assert_bool(health_component.is_alive()).is_equal(false)
  assert_bool(health_component.is_dead()).is_equal(true)
  health_component.queue_free()
+
+
+# Might have to add more test cases
+# Signaling
+# Does not go beyond bounds
